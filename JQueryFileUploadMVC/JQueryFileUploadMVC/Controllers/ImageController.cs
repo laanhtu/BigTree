9606d44bd4f6a2d2f46b5c90eb66c0b49a55adb3
Lã Anh Tú <laanhtu@gmail.com>
2015-04-06 10:38:53 +0200

    Add new project with okay for load and upload file through widget.

---
+++ JQueryFileUploadMVC/JQueryFileUploadMVC/Controllers/ImageController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JQueryFileUploadMVC.Models;
+
+namespace JQueryFileUploadMVC.Controllers
+{
+    public class ImageController : Controller
+    {
+        //
+        // GET: /Image/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Image/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Image/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Image/Create
+
+        [HttpPost]
+        public ActionResult Create(ImageViewModel model)
+        {
+            try
+            {
+                model.Id = new Random().Next(3000);
+
+                return RedirectToAction("Edit", new { Id = model.Id });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Image/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+
+            return View();
+        }
+
+        //
+        // POST: /Image/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Image/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Image/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public void Upload(ImageViewModel model)
+        {
+            Debug.WriteLine("Id: " + model.Id);
+            var context = Request.RequestContext.HttpContext;
+            foreach (var file in context.Request.Files)
+            { 
+
+            }
+            return;
+        }
+    }
+}
---
+++ JQueryFileUploadMVC/JQueryFileUploadMVC/Models/ImageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace JQueryFileUploadMVC.Models
+{
+    public class ImageViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Image path")]
+        public string ImagePath { get; set; }
+        public byte Quantity { get; set; }
+        [DisplayName("Meta data")]
+        public string Metadata { get; set; }
+    }
+}
